3671d4f9854d3baabeacaf7336e2274170a562c1
niku153 <104482078+niku153@users.noreply.github.com>
2022-06-28 14:03:04 +0800

    Added API Integration

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <Weather defaultCity="Perth" />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,204 +1,215 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import axios from "axios";
 
-export default function App() {
-  let weatherData = {
-    city: "Perth",
-    temperature: 19,
-    date: "Monday, 13th June",
-    time: "11:30",
-    description: "Sunny",
-    currentConditionIcon:
-      "https://raw.githubusercontent.com/niku153/Weather-application/main/media/01d.svg",
-    humidity: "30%",
-    wind: "10km/h",
-    rainfall: "0mm",
-    sunrise: "5:25am",
-    sunset: "6:49pm",
-  };
+export default function Weather(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [conditions, setConditions] = useState({});
+  function handleResponse(response) {
+    setConditions({
+      temperature: Math.round(response.data.main.temp),
+      wind: response.data.wind.speed,
+      name: response.data.name,
+      description: response.data.weather[0].main,
+      icon: `https://raw.githubusercontent.com/niku153/Weather-application/main/media/${response.data.weather[0].icon}.svg`,
+      humidity: response.data.main.humidity,
+      rainfall: "0mm",
+      sunrise: "5:25am",
+      sunset: "6:49pm",
+    });
+    setLoaded(true);
+  }
 
-  return (
-    <div className="Weather">
-      <div className="container shadow">
-        <form id="search-form">
-          <div className="row">
-            <div className="col-8">
-              <input
-                type="search"
-                placeholder="Search for a city"
-                className="form-control search-input-box"
-                id="search-input-box"
-                autoComplete="off"
-              />
-            </div>
-            <div className="col-2">
-              <input
-                type="submit"
-                value="Search"
-                className="form-control search-button"
-              />
-            </div>
-            <div className="col-1">
-              <input
-                type="submit"
-                value="📍"
-                className="form-control current-location-button"
-                id="current-location-button"
-              />
-            </div>
-          </div>
-        </form>
-        <div className="row">
-          <div className="col-sm-8 current-conditions shadow">
-            <h1 id="current-city">{weatherData.city}</h1>
-            <h2>
-              <span id="current-date">{weatherData.date}</span>
-              <span className="current-time" id="current-time">
-                {weatherData.time}
-              </span>
-            </h2>
-            <div className="current-temperature-wrapper">
-              <span className="current-temperature" id="current-temperature">
-                {weatherData.temperature}
-              </span>
-              <span className="units">
-                <a href="/" className="celsius" id="celsius">
-                  °C
-                </a>{" "}
-                |
-                <a href="/" className="fahrenheit" id="fahrenheit">
-                  °F
-                </a>
-              </span>
-              <span>
-                <img
-                  src={weatherData.currentConditionIcon}
-                  alt=""
-                  className="current-condition-icon"
-                  id="current-condition-icon"
+  if (loaded) {
+    return (
+      <div className="Weather">
+        <div className="container shadow">
+          <form id="search-form">
+            <div className="row">
+              <div className="col-8">
+                <input
+                  type="search"
+                  placeholder="Search for a city"
+                  className="form-control search-input-box"
+                  id="search-input-box"
+                  autoComplete="off"
                 />
-              </span>
-              <div className="weather-description" id="weather-description">
-                {weatherData.description}
+              </div>
+              <div className="col-2">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="form-control search-button"
+                />
+              </div>
+              <div className="col-1">
+                <input
+                  type="submit"
+                  value="📍"
+                  className="form-control current-location-button"
+                  id="current-location-button"
+                />
               </div>
             </div>
-            <ul className="other-parameters">
-              <li className="Wind">
+          </form>
+          <div className="row">
+            <div className="col-sm-8 current-conditions shadow">
+              <h1 id="current-city">{conditions.name}</h1>
+              <h2>
+                <span id="current-date">5th June</span>
+                <span className="current-time" id="current-time">
+                  0700
+                </span>
+              </h2>
+              <div className="current-temperature-wrapper">
+                <span className="current-temperature" id="current-temperature">
+                  {conditions.temperature}
+                </span>
+                <span className="units">
+                  <a href="/" className="celsius" id="celsius">
+                    °C
+                  </a>{" "}
+                  |
+                  <a href="/" className="fahrenheit" id="fahrenheit">
+                    °F
+                  </a>
+                </span>
                 <span>
                   <img
-                    src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/wind.svg"
-                    alt="wind-icon"
-                    className="other-parameter-icon"
+                    src={conditions.icon}
+                    alt={conditions.description}
+                    className="current-condition-icon"
+                    id="current-condition-icon"
                   />
                 </span>
-                Wind:
-                <span className="wind-value other-parameter-values" id="wind">
-                  {" "}
-                  {weatherData.wind}
-                </span>
-              </li>
-              <li className="Rainfall">
-                <span>
-                  <img
-                    src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/raindrop.svg"
-                    alt="raindrop-icon"
-                    className="other-parameter-icon"
-                  />
-                </span>
-                Rainfall:
-                <span
-                  className="rainfall-value other-parameter-values"
-                  id="rainfall"
-                >
-                  {" "}
-                  {weatherData.rainfall}
-                </span>
-              </li>
-              <li className="Humidity">
-                <span>
-                  <img
-                    src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/thermometer.svg"
-                    alt="humidity-icon"
-                    className="other-parameter-icon"
-                  />
-                </span>
-                Humidity:
-                <span
-                  className="humidity-value other-parameter-values"
-                  id="humidity"
-                >
-                  {" "}
-                  {weatherData.humidity}
-                </span>
-              </li>
-              <li className="Sunrise">
-                <span>
-                  <img
-                    src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/sunrise.svg"
-                    alt="sunrise-icon"
-                    className="other-parameter-icon"
-                  />
-                </span>
-                Sunrise:
-                <span
-                  className="sunrise-time other-parameter-values"
-                  id="sunrise"
-                >
-                  {" "}
-                  {weatherData.sunrise}
-                </span>
-              </li>
-              <li className="Sunset">
-                <span>
-                  <img
-                    src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/sunset.svg"
-                    alt="sunset-icon"
-                    className="other-parameter-icon"
-                  />
-                </span>
-                Sunset:
-                <span
-                  className="sunset-time other-parameter-values"
-                  id="sunset"
-                >
-                  {" "}
-                  {weatherData.sunset}
-                </span>
-              </li>
-            </ul>
-          </div>
+                <div className="weather-description" id="weather-description">
+                  {conditions.description}
+                </div>
+              </div>
+              <ul className="other-parameters">
+                <li className="Wind">
+                  <span>
+                    <img
+                      src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/wind.svg"
+                      alt="wind-icon"
+                      className="other-parameter-icon"
+                    />
+                  </span>
+                  Wind:
+                  <span className="wind-value other-parameter-values" id="wind">
+                    {" "}
+                    {conditions.wind}km/h
+                  </span>
+                </li>
+                <li className="Rainfall">
+                  <span>
+                    <img
+                      src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/raindrop.svg"
+                      alt="raindrop-icon"
+                      className="other-parameter-icon"
+                    />
+                  </span>
+                  Rainfall:
+                  <span
+                    className="rainfall-value other-parameter-values"
+                    id="rainfall"
+                  >
+                    {" "}
+                    {conditions.rainfall}
+                  </span>
+                </li>
+                <li className="Humidity">
+                  <span>
+                    <img
+                      src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/thermometer.svg"
+                      alt="humidity-icon"
+                      className="other-parameter-icon"
+                    />
+                  </span>
+                  Humidity:
+                  <span
+                    className="humidity-value other-parameter-values"
+                    id="humidity"
+                  >
+                    {" "}
+                    {conditions.humidity}%
+                  </span>
+                </li>
+                <li className="Sunrise">
+                  <span>
+                    <img
+                      src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/sunrise.svg"
+                      alt="sunrise-icon"
+                      className="other-parameter-icon"
+                    />
+                  </span>
+                  Sunrise:
+                  <span
+                    className="sunrise-time other-parameter-values"
+                    id="sunrise"
+                  >
+                    {" "}
+                    {conditions.sunrise}
+                  </span>
+                </li>
+                <li className="Sunset">
+                  <span>
+                    <img
+                      src="https://raw.githubusercontent.com/niku153/Weather-application/main/media/sunset.svg"
+                      alt="sunset-icon"
+                      className="other-parameter-icon"
+                    />
+                  </span>
+                  Sunset:
+                  <span
+                    className="sunset-time other-parameter-values"
+                    id="sunset"
+                  >
+                    {" "}
+                    {conditions.sunset}
+                  </span>
+                </li>
+              </ul>
+            </div>
 
-          <div className="col-sm-4 forecast shadow" id="forecast"></div>
+            <div className="col-sm-4 forecast shadow" id="forecast"></div>
+          </div>
         </div>
+        <footer>
+          <a
+            href="https://github.com/niku153/react-weather"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-link"
+          >
+            open source code{" "}
+          </a>
+          by{" "}
+          <a
+            href="https://vermillion-froyo-c5f38e.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-link"
+          >
+            nirali parikh
+          </a>
+          , hosted on{" "}
+          <a
+            href="https://bejewelled-cranachan-510a11.netlify.app/?"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-link"
+          >
+            netlify
+          </a>
+        </footer>
       </div>
-      <footer>
-        <a
-          href="https://github.com/niku153/react-weather"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-link"
-        >
-          open source code{" "}
-        </a>
-        by{" "}
-        <a
-          href="https://vermillion-froyo-c5f38e.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-link"
-        >
-          nirali parikh
-        </a>
-        , hosted on{" "}
-        <a
-          href="https://bejewelled-cranachan-510a11.netlify.app/?"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-link"
-        >
-          netlify
-        </a>
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "06bc256f164c93573001cb99d320c17d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
